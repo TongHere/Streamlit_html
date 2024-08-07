057d3e1e36004f107d10e4949cb531242911cced
Tong <95514305+TongHere@users.noreply.github.com>
2024-08-07 12:44:24 +0200

    change dummy to chatgpt

--- app.py
+++ app.py
@@ -57,44 +57,44 @@ def upload_and_process_keywords_file(uploaded_file):
         print("No CSV file was uploaded.")
         return None
 
-def generate_article_content(keyword, content_length, language,search_intent ):
+def generate_article_content(keyword, content_length, language,search_intent):
     # raise Exception('Upps, article content failed to be generated in generate_article_content')
     # dummy text
     # simulating chatgpt's API response
-    article_content = f'<p>dummy text for keyword {keyword} in language {language} text in bLorem ipsum dolor read {search_intent} sit amet consectetur adipisicing elit. Accusantium, libero omnis perspiciatis animi at similique tempora mollitia in rem soluta.</p>'
-    article_content += f'<h2>heading for keyword {keyword}</h2>'
-    article_content += f'<p>dummy text for keyword {keyword} in language {language} text in bLorem ipsum dolor read {search_intent} sit amet consectetur adipisicing elit. Accusantium, libero omnis perspiciatis animi at similique tempora mollitia in rem soluta.</p>'
-    article_content += f'<h2>heading for keyword {keyword}</h2>'
-    article_content += f'<p>dummy text for keyword {keyword} in language {language} text in bLorem ipsum dolor read {search_intent} sit amet consectetur adipisicing elit. Accusantium, libero omnis perspiciatis animi at similique tempora mollitia in rem soluta.</p>'
+    # article_content = f'<p>dummy text for keyword {keyword} in language {language} text in bLorem ipsum dolor read {search_intent} sit amet consectetur adipisicing elit. Accusantium, libero omnis perspiciatis animi at similique tempora mollitia in rem soluta.</p>'
+    # article_content += f'<h2>heading for keyword {keyword}</h2>'
+    # article_content += f'<p>dummy text for keyword {keyword} in language {language} text in bLorem ipsum dolor read {search_intent} sit amet consectetur adipisicing elit. Accusantium, libero omnis perspiciatis animi at similique tempora mollitia in rem soluta.</p>'
+    # article_content += f'<h2>heading for keyword {keyword}</h2>'
+    # article_content += f'<p>dummy text for keyword {keyword} in language {language} text in bLorem ipsum dolor read {search_intent} sit amet consectetur adipisicing elit. Accusantium, libero omnis perspiciatis animi at similique tempora mollitia in rem soluta.</p>'
 
-    return article_content
-    # llm = ChatOpenAI(model='gpt-4o', temperature=0.7)
+    # return article_content
+    llm = ChatOpenAI(model='gpt-4o', temperature=0.7)
     
-    # prompt_template = """
-    # You are a writer for InstaCams.com, a cam to cam platform.
-    # You are writing for the keyword "{keyword}" and the search intent is "User is looking for alternatives to {keyword}".
-    # Write a {content_length} word article in {language} for InstaCams that fulfils this search intent.
-    # Conclude the article by recommending them to try InstaCams.
-    # The article should be formatted as valid HTML fragment with valid heading and paragraph HTML elements.
-    # Use <h2> as a section header.
-    # Article as valid HTML fragment:
-    # """
+    prompt_template = """
+    You are a writer for InstaCams.com, a cam to cam platform.
+    You are writing for the keyword "{keyword}" and the search intent is "{search_intent}".
+    Write a {content_length} word article in {language} for InstaCams that fulfils this search intent.
+    Conclude the article by recommending them to try InstaCams.
+    The article should be formatted as valid HTML fragment with valid heading and paragraph HTML elements.
+    Use <h2> as a section header.
+    Article as valid HTML fragment:
+    """
     
-    # prompt = PromptTemplate(
-    #     input_variables=["keyword", "content_length", "language"],
-    #     template=prompt_template
-    # )
+    prompt = PromptTemplate(
+        input_variables=["keyword", "content_length", "language","search_intent"],
+        template=prompt_template
+    )
     
-    # chain = LLMChain(llm=llm, prompt=prompt)
+    chain = LLMChain(llm=llm, prompt=prompt)
     
-    # article_content = chain.run(keyword=keyword, content_length=content_length, language=language)
+    article_content = chain.run(keyword=keyword, content_length=content_length, language=language, search_intent=search_intent)
 
     # article content starts with ```html and ends with ```
     # strip these to get only html
-    # article_content_lstripped = article_content.lstrip('```html')
-    # article_content_as_html = article_content_lstripped.rstrip('```')
+    article_content_lstripped = article_content.lstrip('```html')
+    article_content_as_html = article_content_lstripped.rstrip('```')
 
-    # return article_content_as_html
+    return article_content_as_html
 
 def get_relative_path(keyword):
     keyword_lower_case = keyword.lower()
